3d0a3a297a7a508bc8664445fe2244b98dba72ce
jihyun <jihyun@orangefield.co.kr>
2024-07-07 13:30:11 +0900

    feat. spline test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div>
         hello
+        <Spline
+          scene="https://prod.spline.design/7Wi5Hgg-BjjgEj94/scene.splinecode" 
+        />
       </div>
 
     </main>
