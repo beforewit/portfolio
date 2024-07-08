c68622fb868c694221368e956b7f8a4dce8b54aa
Hyun Young Kim <hyunyoungkim@Hyunui-MacBookPro-2.local>
2024-07-09 08:47:19 +0900

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
             <HamburgerMenu/>
           </div>
         </div>
-        {/* <Spline
+        { <Spline
           scene="https://prod.spline.design/7Wi5Hgg-BjjgEj94/scene.splinecode" 
-        /> */}
+        /> }
       </div>
 
     </main>
