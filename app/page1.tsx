c60612713652a016f82058fd663e6dfa9dd58f2e
Hyun Young Kim <hyunyoungkim@Hyunui-MacBookPro-2.local>
2024-07-09 09:07:40 +0900

    deploy test

--- app/page1.tsx
+++ app/page1.tsx
@@ -3,7 +3,7 @@ const Page1: React.FC = () => {
     return (
       <div>
         <h1>Page 1</h1>
-        <p>This is the content of Page 1.</p>
+        <p>This is the content of Page 1..</p>
       </div>
     );
   };
