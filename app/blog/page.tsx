b3bef0e97383a00853d22793846a806a6c4ed903
kimmji <bpond37@gmail.com>
2024-07-11 08:16:24 +0900

    fix. 빌드실패 개선

---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+
+// pages/Blog.tsx
+const Blog: React.FC = () => {
+  return (
+    <main className="flex min-h-screen flex-col bg-White900">
+      <div>
+        <Header/>
+      </div>
+    </main>
+  );
+};
+
+export default Blog;
