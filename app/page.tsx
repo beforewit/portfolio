6ae62cdfc231fc5f5a57c50b16a72bd27f3a822f
kimmji <bpond37@gmail.com>
2024-07-13 22:51:39 +0900

    feat. spline 크기 조정

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen  bg-violet_bg">
         <Header/>
-        <div className='flex-grow flex flex-col justify-center items-center'>
+        <div className='spline flex-grow flex flex-col justify-center items-center'>
           <Spline
             className='contents'
             scene="https://prod.spline.design/zq3CjWUJWSfhIFor/scene.splinecode" 
